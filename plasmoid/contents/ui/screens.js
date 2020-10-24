a7f9996b02e522f113f3245bd5d1ac7de0204a36
Šimon Brandner <simon.bra.ag@gmail.com>
2020-10-24 13:25:02 +0200

    Reformatted with spaces

--- plasmoid/contents/ui/screens.js
+++ plasmoid/contents/ui/screens.js
@@ -1,31 +1,31 @@
 function saveConfig() {
-	cfg_screenNameList = [];
-	cfg_screenColorList = [];
-	for (var i = 0; i < screenListModel.count; i++) {
-		cfg_screenNameList.push(screenListModel.get(i).screenName);
-		cfg_screenColorList.push(screenListModel.get(i).screenColor);
-	}
+    cfg_screenNameList = [];
+    cfg_screenColorList = [];
+    for (var i = 0; i < screenListModel.count; i++) {
+        cfg_screenNameList.push(screenListModel.get(i).screenName);
+        cfg_screenColorList.push(screenListModel.get(i).screenColor);
+    }
 }
 
 function loadConfig() {
-	for (var i = 0; i < cfg_screenNameList.length; i++) {
-		screenListModel.append({ screenName: cfg_screenNameList[i], screenColor: cfg_screenColorList[i] });
-	}
+    for (var i = 0; i < cfg_screenNameList.length; i++) {
+        screenListModel.append({screenName: cfg_screenNameList[i], screenColor: cfg_screenColorList[i]});
+    }
 }
 
 function loadNewScreens() {
-	for (var screenName of i3pager.getScreenNames()) {
-		if (!isScreenNameOnList(screenName, cfg_screenNameList)) {
-			screenListModel.append({ screenName: screenName, screenColor: "#000000" });
-		}
-	}
+    for (var screenName of i3pager.getScreenNames()) {
+        if (!isScreenNameOnList(screenName, cfg_screenNameList)) {
+            screenListModel.append({screenName: screenName, screenColor: "#000000"});
+        }
+    }
 }
 
 function isScreenNameOnList(name, list) {
-	for (var screenName of list) {
-		if (name == screenName) {
-			return true;
-		}
-	}
-	return false;
+    for (var screenName of list) {
+        if (name == screenName) {
+            return true;
+        }
+    }
+    return false;
 }
